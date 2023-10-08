c58d426e1e08a8c59941c2101602506dc1dd7f5a
orla11 <orlandif.mail@gmail.com>
2023-10-08 20:24:45 +0200

    fixed import error

--- ctf_team_bandit/ctf_team_bandit.py
+++ ctf_team_bandit/ctf_team_bandit.py
@@ -1,21 +1,11 @@
 import autogen
 from config import llm_config
-from ctf_team_bandit.team import user_proxy, hacker_1, hacker_2, reporter, manager
+from team import user_proxy, hacker_1, hacker_2, reporter, manager
 
 agents = [user_proxy, manager, hacker_1, hacker_2, reporter]
 
 groupchat = autogen.GroupChat(agents=agents, messages=[], max_round=120)
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
-
-# message = """
-#     As a well organized and espert hacking team, your goal is to try to complete
-#     all the levels of the challenge OverTheWire Bandit Wargame: https://overthewire.org/wargames/bandit/.
-#     Try to solve the problems with coding and report your results.
-#     Start by going to Level 0 page: https://overthewire.org/wargames/bandit/bandit0.html,
-#     try to undertand what is the problem and hack your way up to a level given as input by User_proxy.
-#     Once the desired level is completed, at that point, Reporter will provide me a summary on the job done.
-#     DO NOT DO ANYTHING ELSE THAN FINDING YOUR WAY THROUGH THE CHALLENGE USING PYTHON CODING. Start.
-# """
 
 message = """
     As an organized and skilled hacking team, your objective is to successfully complete all the levels in the 
